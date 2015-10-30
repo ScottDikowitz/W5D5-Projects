1704079c375eb25df100cf9aa050e2437cdafe70
App Academy Student <appacademy@Apps-Mac-mini-264.local>
2015-10-30 12:10:00 -0400

    Initial commit, add clock

---
+++ timing.js
@@ -0,0 +1,24 @@
+function Clock(currentTime) {
+  //currentTime is date Object
+  this.currentTime = -1;
+}
+
+Clock.prototype.printTime = function() {
+  timeString = this.currentTime.getHours() + ":" + this.currentTime.getMinutes() + ":" + this.currentTime.getSeconds();
+  console.log(timeString);
+}
+
+Clock.prototype.run = function() {
+  this.currentTime = new Date();
+  this.printTime();
+    window.setInterval(this._tick.bind(this), 5000);
+  }
+
+Clock.prototype._tick = function() {
+  var curSeconds = this.currentTime.getSeconds();
+  this.currentTime.setSeconds(curSeconds + 5);
+  this.printTime();
+}
+
+var clock = new Clock();
+clock.run();
